62ce4c8c43d6efb1e6b2f36ea95397b070295f29
Sakuilim <misterism3@gmail.com>
2023-02-23 21:08:08 +0200

    Roslintor-started-working-on-performance-practice-usage-analyzer

---
+++ Roslintor/Roslintor.Tests/PerformancePracticeAnalyzerTests.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VerifyCS = Roslintor.Test.CSharpCodeFixVerifier<
+    Roslintor.NamingAnalyzers.PerformancePracticeAnalyzer,
+    Roslintor.NamingCodeFix.RoslintorCodeFixProvider>;
+
+namespace Roslintor.Tests
+{
+    [TestClass]
+    public class PerformancePracticeAnalyzerTests
+    {
+        [TestMethod]
+        public async Task PerformancePractice_Wrong_ShouldNotShowWarning()
+        {
+            var test = @"
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+            using System.Diagnostics;
+            using System.Collections;
+
+            namespace ConsoleApplication1
+            {
+                public class TestClass
+                {   
+                    public void {|#0:MethodName|}(string name)
+                    {
+                        Hashtable ht = new Hashtable();
+                        
+                        ht.Add(""001"", ""MeLOL"");
+
+                    }    
+                }
+            }";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        [TestMethod]
+        public async Task PerformancePractice_Wrong_ShouldShowWarning()
+        {
+            var test = @"
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+            using System.Diagnostics;
+            using System.Collections;
+
+            namespace ConsoleApplication1
+            {
+                public class TestClass
+                {   
+                    public void Method(string name)
+                    {
+                        var list = new List<string> { ""foo"", ""bar"", ""baz"" };
+                        var searchTerm = ""baz"";
+                        var index = list.IndexOf(searchTerm); // This will trigger a diagnostic because List.IndexOf is less efficient than using a HashSet or Dictionary.
+                    }    
+                }
+            }";
+
+            var expected = VerifyCS.Diagnostic("PPA01");
+
+            Console.WriteLine(expected);
+
+            await VerifyCS.VerifyAnalyzerAsync(test, expected);
+        }
+    }
+}
---
+++ Roslintor/Roslintor/NamingAnalyzers/PerformancePracticeAnalyzer.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Roslintor.Helper;
+
+namespace Roslintor.NamingAnalyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PerformancePracticeAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "PPA01";
+        private const string Title = "Use a Hash Table for Searching";
+        private const string MessageFormat = "Consider using a hash table for searching instead of a list.";
+        private const string Category = "Performance";
+        private const string Description = "Using a hash table can improve search performance.";
+
+        private static readonly DiagnosticDescriptor Rule =
+       new DiagnosticDescriptor(
+           DiagnosticId,
+           Title,
+           MessageFormat,
+           Category,
+           DiagnosticSeverity.Warning,
+           isEnabledByDefault: true,
+           description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.InvocationExpression);
+        }
+
+        private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+        {
+            var invocation = context.Node as InvocationExpressionSyntax;
+
+            if (invocation != null &&
+                invocation.Expression is MemberAccessExpressionSyntax memberAccess &&
+                memberAccess.Name.Identifier.ValueText.Equals("IndexOf"))
+            {
+                var symbolInfo = context.SemanticModel.GetSymbolInfo(memberAccess.Name);
+                if (symbolInfo.Symbol != null &&
+                    symbolInfo.Symbol.ContainingType.SpecialType == SpecialType.System_Collections_Generic_IList_T)
+                {
+                    var diagnostic = Diagnostic.Create(Rule, memberAccess.Name.GetLocation());
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+
+        private void AnalyzeInvocation(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is InvocationExpressionSyntax invocation &&
+                invocation.Expression is MemberAccessExpressionSyntax memberAccess &&
+                memberAccess.Name.Identifier.ValueText == "IndexOf" &&
+                context.SemanticModel.GetSymbolInfo(memberAccess).Symbol is IMethodSymbol methodSymbol &&
+                methodSymbol.ContainingType?.SpecialType == SpecialType.System_Collections_Generic_IList_T)
+            {
+                var diagnostic = Diagnostic.Create(Rule, memberAccess.GetLocation());
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
